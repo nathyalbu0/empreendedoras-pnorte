da7de84950a7337af1e7d3afe3c57200719f4559
nathyalbu0 <nathalyalbudf@gmail.com>
2026-05-12 10:18:37 -0300

    Corrige URL da API para usar variável de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -9,7 +9,6 @@ const api = axios.create({
   }
 });
 
-// Interceptor para adicionar token nas requisições
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('@PNorte:token');
@@ -23,7 +22,6 @@ api.interceptors.request.use(
   }
 );
 
-// Interceptor para tratar erros de autenticação
 api.interceptors.response.use(
   (response) => response,
   (error) => {
